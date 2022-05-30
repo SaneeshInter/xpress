9ac6e0c5a120599ec742ed0c809a754018cc1d8a
rashidvv <101393154+rashidvv@users.noreply.github.com>
2022-05-30 10:39:11 +0530

    manager_home_screen.dart manager_home_bloc.dart strings.dart (tested)

--- lib/blocs/manager_home_bloc.dart
+++ lib/blocs/manager_home_bloc.dart
@@ -7,11 +7,15 @@ class ManagaerHomeBloc {
   final _repo = Repository();
   final _managerhome = PublishSubject<ManagerHomeResponse>();
 
+  final _visibility = PublishSubject<bool>();
+  Stream<bool> get visible => _visibility.stream;
+
   Stream<ManagerHomeResponse> get managerhomeStream => _managerhome.stream;
 
-  fetchManagerHome(token) async {
+  fetchManagerHome(token) async {  _visibility.add(true);
     ManagerHomeResponse list = await _repo.fetchManagerHomeResponse(token);
     _managerhome.sink.add(list);
+  _visibility.add(false);
   }
 
   dispose() {
